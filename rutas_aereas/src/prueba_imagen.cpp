4c66eb9e38e9ed1839ac7700ba1ecdbbfa89dd83
Isaac Morales <imorsan@correo.ugr.es>
2023-11-28 18:30:41 +0100

    Trabajando sobre imagen.cpp e imagen.h

---
+++ rutas_aereas/src/prueba_imagen.cpp
@@ -0,0 +1,16 @@
+//
+// Created by isaacm on 28/11/23.
+//
+
+#include "imagen.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main(int argc, char * argv[]){
+
+    Imagen I;
+
+    I.LeerImagen(argv[1]);
+
+    I.EscribirImagen("prueba.ppm");
+}
