ed337720fcdcefc21ad0f9d1daf9524817774f29
patriiciacoboss <pcobosrueda@correo.ugr.es>
2023-11-30 13:04:44 +0100

    falta terminar de paises.cpp el ostream que ns por que da error el de la profesora y en ruta.cpp no me deja definir it :(

--- rutas_aereas/include/AlmacenRutas.h
+++ rutas_aereas/include/AlmacenRutas.h
@@ -4,6 +4,51 @@
 
 #ifndef PRACTICAFINALED_ALMACENRUTAS_H
 #define PRACTICAFINALED_ALMACENRUTAS_H
+#include "Ruta.h"
+#include "map"
+#include "string"
 
+class AlmacenRutas{
+private:
+    map<string, Ruta> rutas;
+public:
+    AlmacenRutas(){}
+    void Insertar(const Ruta &r);
+    void Borrar(const Ruta &r);
+    Ruta getRuta(const string &a);
+
+    class iterator{
+    private:
+        map<string, Ruta>::iterator p;
+    public:
+        iterator(){}
+        bool operator == (AlmacenRutas &almacen);
+        bool operator != (AlmacenRutas &almacen);
+        iterator operator ++();
+        iterator operator *();
+
+    };
+
+    class const_iterator{
+    private:
+        map<string, Ruta>::const_iterator p;
+    public:
+        const_iterator(){}
+        bool operator == (AlmacenRutas &almacen);
+        bool operator != (AlmacenRutas &almacen);
+        const_iterator operator ++();
+        const_iterator operator *();
+
+    };
+
+    iterator begin();
+    const_iterator begin() const;
+    iterator end();
+    const_iterator end() const;
+
+    friend istream & operator >> (istream & is, AlmacenRutas &almacen);
+    friend ostream & operator << (ostream & is, const AlmacenRutas &almacen);
+
+};
 
 #endif //PRACTICAFINALED_ALMACENRUTAS_H
--- rutas_aereas/src/AlmacenRutas.cpp
+++ rutas_aereas/src/AlmacenRutas.cpp
@@ -6,3 +6,31 @@
 #include "AlmacenRutas.h"
 using namespace std;
 
+void AlmacenRutas::Insertar(const Ruta &r){}
+void AlmacenRutas::Borrar(const Ruta &r){}
+Ruta AlmacenRutas::getRuta(const string &a){}
+
+bool AlmacenRutas::iterator::operator == (AlmacenRutas &almacen){}
+bool AlmacenRutas::iterator::operator != (AlmacenRutas &almacen){}
+AlmacenRutas::iterator AlmacenRutas::iterator::operator ++(){}
+AlmacenRutas::iterator AlmacenRutas::iterator::operator *(){}
+
+bool AlmacenRutas::const_iterator::operator == (AlmacenRutas &almacen){}
+bool AlmacenRutas::const_iterator::operator != (AlmacenRutas &almacen){}
+AlmacenRutas::const_iterator AlmacenRutas::const_iterator::operator ++(){}
+AlmacenRutas::const_iterator AlmacenRutas::const_iterator::operator *(){}
+
+
+
+AlmacenRutas::iterator AlmacenRutas::begin(){
+    return rutas.begin();
+}
+AlmacenRutas::const_iterator AlmacenRutas::begin() const{
+    rutas.cbegin(); //Por que puedo hacer esto
+    return rutas.cbegin(); //y esto no
+}
+AlmacenRutas::iterator AlmacenRutas::end(){}
+AlmacenRutas::const_iterator AlmacenRutas::end() const{}
+
+istream & operator >> (istream & is, AlmacenRutas &almacen){}
+ostream & operator << (ostream & is, const AlmacenRutas &almacen){}
