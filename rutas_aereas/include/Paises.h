be0c6324570c7122db8d6a31e556a482663f8097
Isaac Morales <imorsan@correo.ugr.es>
2023-12-11 23:53:38 +0100

    Formateado el codigo

--- rutas_aereas/include/Paises.h
+++ rutas_aereas/include/Paises.h
@@ -8,39 +8,49 @@
 
 #include "Pais.h"
 #include <set>
+
 using namespace std;
 
-class Paises{
+class Paises {
 private:
     set<Pais> datos;
 public:
-    Paises(){}
+    Paises() {}
+
     void Insertar(const Pais &P);
+
     void Borrar(const Pais &P);
 
-    class const_iterator;
-    class iterator{
+    class iterator {
     private:
         set<Pais>::iterator p;
     public:
         friend class Paises;
-        friend class const_iterator;
 
-        iterator(){}
-        bool operator != (Pais &pais);
-        bool operator == (Pais &pais);
-        iterator operator ++ ();
-        iterator operator * ();
+        iterator() {}
+
+        bool operator!=(Pais &pais);
+
+        bool operator==(Pais &pais);
+
+        iterator operator++();
+
+        iterator operator*();
     };
-    class const_iterator{
+
+    class const_iterator {
     private:
         set<Pais>::const_iterator p;
     public:
         const_iterator();
-        bool operator != (Pais &pais);
-        bool operator == (Pais &pais);
-        const_iterator operator ++ ();
-        const_iterator operator * () const;
+
+        bool operator!=(Pais &pais);
+
+        bool operator==(Pais &pais);
+
+        const_iterator operator++();
+
+        const_iterator operator*() const;
 
         friend class Paises;
 
@@ -48,16 +58,21 @@ public:
     };
 
     iterator begin();
-    const_iterator begin()const;
+
+    const_iterator begin() const;
+
     iterator end();
-    const_iterator end()const;
+
+    const_iterator end() const;
+
     iterator find(const Pais &p);
+
     iterator find(const Punto &p);
 
-    friend istream & operator>>(istream & is, Paises & R);
-    friend ostream & operator<<(ostream & os, const Paises &R);
-};
+    friend istream &operator>>(istream &is, Paises &R);
 
+    friend ostream &operator<<(ostream &os, const Paises &R);
+};
 
 
 #endif //PRACTICAFINALED_PAISES_H
